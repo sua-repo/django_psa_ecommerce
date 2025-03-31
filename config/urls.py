441672603d70d9340b08319d4e2f10c0c5c7706a
sua <dssua0714@naver.com>
2025-03-31 13:29:32 +0900

    about 페이지 구현

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -8,5 +8,11 @@ urlpatterns = [
     path("", include("store.urls")),  # dev_1
 ]
 
+# dev_2
+# http://127.0.0.1:8000/media/파일경로
+# MEDIA_URL = "media/"  # ex) /media/photo1.png
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# dev_2
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = "store"
+
 # dev_1
 urlpatterns = [
     path("", views.home, name="home"),  # dev_1
+    path("about/", views.about, name="about"),  # dev_8
 ]
