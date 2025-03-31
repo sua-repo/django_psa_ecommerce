51fe020dab7752660b99001b5ba9a8ce484ba40c
sua <dssua0714@naver.com>
2025-03-31 11:09:45 +0900

    세일 처리

--- store/models.py
+++ store/models.py
@@ -20,6 +20,8 @@ class Product(models.Model):
     description = models.CharField(max_length=250, default="", blank=True, null=True)
     image = models.ImageField(upload_to="upload/product/")
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    is_sale = models.BooleanField(default=False)  # dev_6
+    sale_price = models.IntegerField(default=0)  # dev_6
 
     def __str__(self):
         return self.name
