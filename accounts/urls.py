18184c4b5dfe1564608433d08d9d68ca31a59ef4
sua <dssua0714@naver.com>
2025-04-01 11:27:44 +0900

    로그인 로그아웃

---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+
+from accounts import views
+
+# dev_9
+
+app_name = "accounts"
+
+urlpatterns = [
+    path("login/", views.login_user, name="login_user"),  # dev_9
+    path("logout", views.logout_user, name="logout_user"),  # dev_9
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+
 
 # Create your views here.
+
+# class HTTPRequest :
+#   POST = {"username" : "admin", "password" : "1234"}
+
+
+# dev_9
+def login_user(request):
+
+    if request.method == "POST":
+        # username = request.POST.get("username", "") 가 더 안전 / username이 없으면 빈 칸으로 대체
+        username = request.POST["username"]  # username이 없으면 죽음
+        password = request.POST["password"]
+
+        # DB에 있는지를 조회하고 대조
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "로그인이 되었습니다.")
+            return redirect("/")
+
+        else:
+            messages.success(
+                request, "로그인이 실해했습니다. 다시 한 번 더 시도해주시기 바랍니다."
+            )
+            # return redirect("accounts:login_user")
+            return redirect("accounts/login/")
+    else:
+        return render(request, "accounts/login.html", {})
+
+
+# dev_9
+def logout_user(request):
+    logout(request)  # session에 저장된 sessionid 삭제
+    return redirect("/")
