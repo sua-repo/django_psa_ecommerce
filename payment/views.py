9c28244ece83ea532766d3b1b63527320b353d89
sua <dssua0714@naver.com>
2025-04-09 14:44:12 +0900

    주문처리 및 결제

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+# dev_26
+@login_required(login_url="accounts:login_user")
+def payment_process(request):
+    pass
